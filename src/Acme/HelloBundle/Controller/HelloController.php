96959bc5b9febe779cf99a60f8a45fdef741a8b1
foobar <foobar@foobar.(none)>
2012-10-01 21:36:56 +0200

    using twig template

--- src/Acme/HelloBundle/Controller/HelloController.php
+++ src/Acme/HelloBundle/Controller/HelloController.php
@@ -2,9 +2,13 @@
 namespace Acme\HelloBundle\Controller;
 
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
-class HelloController {
+class HelloController extends Controller {
 	public function indexAction($name) {
-		return new Response('<html><body>Hello '.$name.'</body></html>');
+		return $this->render(
+					'AcmeHelloBundle:Hello:index.html.twig',
+					array('name' => $name)
+				);
 	}
 }
