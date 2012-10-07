9006ddf57e57550d49e5d1d365fbfe3399db965c
foobar <foobar@foobar.(none)>
2012-10-07 03:51:21 +0200

    new testbundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -20,6 +20,7 @@ class AppKernel extends Kernel
             new JMS\DiExtraBundle\JMSDiExtraBundle($this),
             new JMS\SecurityExtraBundle\JMSSecurityExtraBundle(),
 			new Acme\HelloBundle\AcmeHelloBundle(),
+			new Acme\TestBundle\TestBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
---
+++ src/Acme/TestBundle/TestBundle.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Acme\TestBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class TestBundle extends Bundle
+{
+	
+}
