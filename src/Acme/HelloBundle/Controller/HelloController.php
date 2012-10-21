0b4557230ca8b680d02e877b54cb8b6456bfca35
foobar <foobar@foobar.(none)>
2012-10-21 20:44:49 +0200

    router with muitiple named parameters default parameters Request object

--- src/Acme/HelloBundle/Controller/HelloController.php
+++ src/Acme/HelloBundle/Controller/HelloController.php
@@ -2,13 +2,14 @@
 namespace Acme\HelloBundle\Controller;
 
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class HelloController extends Controller {
-	public function indexAction($name) {
+	public function indexAction(Request $request, $first_name, $last_name, $color) {
 		return $this->render(
 					'AcmeHelloBundle:Hello:index.html.twig',
-					array('name' => $name)
+					array('name' => $first_name)
 				);
 	}
 }
