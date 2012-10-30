9649d29bde6263ff0e282671f4c1d69882619217
tcial <tcial@tcial.org>
2012-10-30 23:17:05 +0100

    blog controller empty 2

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -19,8 +19,9 @@ class AppKernel extends Kernel
             new JMS\AopBundle\JMSAopBundle(),
             new JMS\DiExtraBundle\JMSDiExtraBundle($this),
             new JMS\SecurityExtraBundle\JMSSecurityExtraBundle(),
-			new Acme\HelloBundle\AcmeHelloBundle(),
-			new Acme\TestBundle\TestBundle(),
+            new Acme\HelloBundle\AcmeHelloBundle(),
+            new Acme\TestBundle\TestBundle(),
+            new Acme\BlogBundle\BlogBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
@@ -35,6 +36,6 @@ class AppKernel extends Kernel
 
     public function registerContainerConfiguration(LoaderInterface $loader)
     {
-        $loader->load(__DIR__.'/config/config_'.$this->getEnvironment().'.yml');
+        $loader->load(__DIR__ . '/config/config_' . $this->getEnvironment() . '.yml');
     }
 }
--- src/Acme/BlogBundle/Controller/BlogController.php
+++ src/Acme/BlogBundle/Controller/BlogController.php
@@ -1,13 +1,13 @@
 <?php
 
-namespace \Acme\BlogBundle\Controller;
+namespace Acme\BlogBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class BlogController extends Controller
 {
-    public function show()
+    public function showAction($slug)
     {
-
+        return $this->render('BlogBundle:Blog:show.html.twig');
     }
 }
