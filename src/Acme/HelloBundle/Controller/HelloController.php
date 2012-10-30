a30f97194e27fd4bdbf6c078584f7ba35506993f
tcial <tcial@tcial.org>
2012-10-30 22:07:44 +0100

    formatting

--- src/Acme/HelloBundle/Controller/HelloController.php
+++ src/Acme/HelloBundle/Controller/HelloController.php
@@ -1,22 +1,26 @@
 <?php
-namespace Acme\HelloBundle\Controller;
+    namespace Acme\HelloBundle\Controller;
 
-use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\HttpFoundation\Request;
-use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+    use Symfony\Component\HttpFoundation\Response;
+    use Symfony\Component\HttpFoundation\Request;
+    use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
-class HelloController extends Controller {
-	public function indexAction(Request $request, $first_name, $last_name, $color) {
-		return $this->forward(
-					'AcmeHelloBundle:Hello:fancy',
-					array('name' => $first_name, 'color' => $color)
-				);
-	}
+    class HelloController extends Controller
+    {
+        public function indexAction(Request $request, $first_name, $last_name, $color)
+        {
+            return $this->forward(
+                'AcmeHelloBundle:Hello:fancy',
+                array('name' => $session->get('foo', 'default'), 'color' => $color)
+            );
+        }
 
-	public function fancyAction($name, $color) {
-		return $this->render(
-					'AcmeHelloBundle:Hello:index.html.twig',
-					array('name' => $name)
-				);
-	}
-}
+        public function fancyAction($name, $color)
+        {
+            return $this->render(
+                'AcmeHelloBundle:Hello:index.html.twig',
+                array('name' => $name)
+            );
+        }
+    }
+
