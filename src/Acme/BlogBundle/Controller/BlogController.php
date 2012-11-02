50e146a5f1299d0edd21b14d324d687307108b6d
tcial <tcial@tcial.org>
2012-11-02 23:17:33 +0100

    requirements in routes

--- src/Acme/BlogBundle/Controller/BlogController.php
+++ src/Acme/BlogBundle/Controller/BlogController.php
@@ -3,14 +3,25 @@
 namespace Acme\BlogBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
 
 class BlogController extends Controller
 {
+    public function indexAction($page)
+    {
+        return new Response("page $page");
+    }
+
     public function showAction($slug)
     {
         return $this->render(
             'BlogBundle:Blog:show.html.twig',
             array('slug' => $slug)
         );
+    }
+
+    public function homepageAction($culture)
+    {
+        return new Response("homepage with culture $culture");
     }
 }
