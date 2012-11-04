fdcb69f986eb4941101e9d6ff6f4939b482aca1b
tcial <tcial@tcial.org>
2012-11-04 13:15:21 +0100

    moved from article model to blog model

--- src/Acme/BlogBundle/Controller/ArticleController.php
+++ src/Acme/BlogBundle/Controller/ArticleController.php
@@ -10,45 +10,4 @@ class ArticleController extends Controller
     {
         return new Response("show article $_route $_controller");
     }
-
-    public function genUrlAction()
-    {
-        $router       = $this->get('router');
-        $router_match = $router->match('/articles/fr/2000/title');
-        $router_match = print_r($router_match, true);
-
-        $uri = $router->generate(
-            'article_show',
-            array(
-                'culture' => 'de',
-                'year'    => 1000,
-                'title'   => 'generated'
-            )
-        );
-
-        $uri_absolute = $router->generate(
-            'article_show',
-            array(
-                'culture' => 'de',
-                'year'    => 1000,
-                'title'   => 'generated'
-            ),
-            true
-        );
-
-        $router->getContext()->setHost('anotherhost.com');
-        $uri_absolute_host = $router->generate(
-            'article_show',
-            array(
-                'culture' => 'de',
-                'year'    => 1000,
-                'title'   => 'generated'
-            ),
-            true
-        );
-
-        return new Response(
-            "$router_match<br>$uri<br>$uri_absolute<br>$uri_absolute_host"
-        );
-    }
 }
--- src/Acme/BlogBundle/Controller/BlogController.php
+++ src/Acme/BlogBundle/Controller/BlogController.php
@@ -12,6 +12,7 @@ class BlogController extends Controller
         return new Response("page $page");
     }
 
+
     public function showAction($slug)
     {
         return $this->render(
@@ -31,8 +32,57 @@ class BlogController extends Controller
         return new Response('contact via get');
     }
 
+
     public function contactProcessAction()
     {
         return new Response('contact via post');
+    }
+
+
+    public function genUrlAction()
+    {
+        $router       = $this->get('router');
+        $router_match = $router->match('/articles/fr/2000/title');
+        $router_match = print_r($router_match, true);
+
+        $uri = $router->generate(
+            'article_show',
+            array(
+                'culture' => 'de',
+                'year'    => 1000,
+                'title'   => 'generated'
+            )
+        );
+
+        $uri_absolute = $router->generate(
+            'article_show',
+            array(
+                'culture' => 'de',
+                'year'    => 1000,
+                'title'   => 'generated'
+            ),
+            true
+        );
+
+        $router->getContext()->setHost('anotherhost.com');
+        $uri_absolute_host = $router->generate(
+            'article_show',
+            array(
+                'culture' => 'de',
+                'year'    => 1000,
+                'title'   => 'generated'
+            ),
+            true
+        );
+
+        return new Response(
+            "$router_match<br>$uri<br>$uri_absolute<br>$uri_absolute_host"
+        );
+    }
+
+
+    public function genUrlTemplateAction()
+    {
+        return $this->render('BlogBundle:Blog:genroutes.html.twig');
     }
 }
