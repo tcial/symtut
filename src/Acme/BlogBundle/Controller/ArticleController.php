6e08593c6e95279ec4264a9c407c2edeadd976e2
tcial <tcial@tcial.org>
2012-11-16 23:18:44 +0100

    include other templates

--- src/Acme/BlogBundle/Controller/ArticleController.php
+++ src/Acme/BlogBundle/Controller/ArticleController.php
@@ -6,6 +6,24 @@ use Symfony\Component\HttpFoundation\Response;
 
 class ArticleController extends Controller
 {
+    protected $articles = array(
+        array(
+            'title'   => 'first title',
+            'author'  => 'first author',
+            'content' => 'first content'
+        ),
+        array(
+            'title'   => 'second title',
+            'author'  => 'second author',
+            'content' => 'second content'
+        ),
+        array(
+            'title'   => 'third title',
+            'author'  => 'third author',
+            'content' => 'third content'
+        ),
+    );
+
     public function showAction($_route, $_controller)
     {
         return new Response("show article $_route $_controller");
@@ -16,11 +34,17 @@ class ArticleController extends Controller
         return $this->render(
             'BlogBundle:Blog:articledetails.html.twig',
             array(
-                'article' => array(
-                    'title'   => 'articletitle',
-                    'author'  => 'articleauthor',
-                    'content' => 'articlecontent'
-                )
+                'article' => $this->articles[0]
+            )
+        );
+    }
+
+    public function listArticlesAction()
+    {
+        return $this->render(
+            'BlogBundle:Blog:articlelist.html.twig',
+            array(
+                'articles' => $this->articles
             )
         );
     }
