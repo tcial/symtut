dd832b77392c6247384b159ee1adc353577b3017
tcial <tcial@tcial.org>
2012-11-17 17:54:40 +0100

    create store action

--- src/Acme/StoreBundle/Controller/DefaultController.php
+++ src/Acme/StoreBundle/Controller/DefaultController.php
@@ -3,11 +3,27 @@
 namespace Acme\StoreBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Acme\StoreBundle\Entity\Product;
+use Symfony\Component\HttpFoundation\Response;
 
 class DefaultController extends Controller
 {
     public function indexAction()
     {
         return $this->render('AcmeStoreBundle:Default:index.html.twig');
+    }
+
+    public function createAction()
+    {
+        $product = new Product();
+        $product->setName('The First Product');
+        $product->setPrice(13.37);
+        $product->setDescription('The first description of the product');
+
+        $em = $this->getDoctrine()->getManager();
+        $em->persist($product);
+        $em->flush();
+
+        return new Response('Created product id '.$product->getId());
     }
 }
